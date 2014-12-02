3cb53d2acb6bc9da334d90d6ec5c8c6686baf1c7
mguzman <marcelom.guzman@jalasoft.com>
2014-12-02 11:45:59 -0400

    Added steps/features

---
+++ features/steps/step_tutorial10.py
@@ -0,0 +1,22 @@
+# file:features/steps/step_tutorial10.py
+# ----------------------------------------------------------------------------
+# STEPS:
+# ----------------------------------------------------------------------------
+from behave   import given, when, then
+from hamcrest import assert_that, equal_to
+from calculator import Calculator
+
+@given('I have a calculator')
+def step_impl(context):
+    context.calculator = Calculator()
+
+@when('I add "{x}" and "{y}"')
+def step_impl(context, x, y):
+    assert isinstance(x, int)
+    assert isinstance(y, int)
+    context.calculator.add2(x, y)
+
+@then('the calculator returns "{expected}"')
+def step_impl(context, expected):
+    assert isinstance(expected, int)
+    assert_that(context.calculator.result, equal_to(expected))
